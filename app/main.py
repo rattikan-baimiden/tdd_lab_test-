997a9fcb6ff9e51bfa1e11d792e904ff3557cae3
Rattikan Baimiden <117657803+rattikan-baimiden@users.noreply.github.com>
2023-03-22 13:43:14 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,7 +7,6 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World1"}
 
-
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
@@ -15,5 +14,10 @@ def read_item(item_id: int, q: str = None):
 @app.get("/hello/{name}")
 def read_name(name: str = None):
     return {"hello": name}
+
+@app.get("/callname/{name}")
+def read_name(name: str = None):
+    return {“hello”: {name}}
+
 
 handler = Mangum(app)
